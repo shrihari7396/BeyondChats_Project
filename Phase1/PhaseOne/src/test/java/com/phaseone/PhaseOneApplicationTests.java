a35541c780838ec6f348774a99b09d48212a8b9f
shrihari7396 <shriharik121@gmail.com>
2025-12-29 14:16:33 +0530

    phase1 started

---
+++ Phase1/PhaseOne/src/test/java/com/phaseone/PhaseOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phaseone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaseOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
